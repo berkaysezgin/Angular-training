b06432f7b9c993720ae81e7b2cad25d65fb8ea87
berkay.sezgin@etiya.com <berkay.sezgin@etiya.com>
2023-01-26 10:57:07 +0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RoleComponent } from './role/role.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+
+const routes: Routes = [{
+  path:"home",
+  component:HomeComponent
+},
+{
+  path:"role",
+  component:RoleComponent
+},
+{
+  path:"user",
+  component:UserComponent
+},{
+  path:"user/:id",
+  component:UserDetailComponent
+},
+{
+  path:"**",
+  component:HomeComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
