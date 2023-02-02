6e2d59f1619a0d5beb5720e92be021b98c9cb327
berkay.sezgin@etiya.com <berkay.sezgin@etiya.com>
2023-02-02 12:47:37 +0300

    created storage structures

---
+++ src/app/storage/storage.module.ts
@@ -0,0 +1,34 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { CookieComponent } from './cookie/cookie.component';
+import { FormsModule } from '@angular/forms';
+import { LocalStorageComponent } from './local-storage/local-storage.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'cookie',
+        component: CookieComponent,
+      },
+      {
+        path: 'local-storage',
+        component: LocalStorageComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'local-storage',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [LocalStorageComponent, CookieComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), FormsModule],
+})
+export class StorageModule {}
